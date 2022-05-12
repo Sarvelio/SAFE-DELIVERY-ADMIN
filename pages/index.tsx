d792ee2fa0b2ce441145fefff055d89eead01668
Sarvelio <gdeleond3@miumg.edu.gt>
2022-05-11 20:01:16 -0600

    commit initial

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <h1>SAFE DELIVERY ADMIN</h1>
+    </div>
+  );
+};
+
+export default Home;
